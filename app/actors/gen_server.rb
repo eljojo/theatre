2cf11cedce162f9d90ef28d09d1da5f751de1f40
José Albornoz <jojo@eljojo.net>
2020-07-14 00:04:45 -0400

    persist messages in database

--- app/actors/gen_server.rb
+++ app/actors/gen_server.rb
@@ -15,15 +15,33 @@ class GenServer
     end
 
     def call(id, action, params = {})
-      record = Actor.find(id)
-      server = new(id, record.state)
-      logger.debug("[#{name}][id=#{id}] action=#{action.inspect} params=#{params.inspect}")
+      Message.create!(
+        actor_id: id,
+        action: action,
+        params: params
+      )
+      nil
+    end
+
+    def process(id)
+      Actor.transaction do
+        actor = Actor.lock.find(id)
+        message = actor.messages.to_process.first
+        process_message!(actor, message) if message
+      end
+    end
+
+    def process_message!(actor, message)
+      action, params = message.action, message.params
+      logger.debug("[#{name}][id=#{actor.id}] action=#{action.inspect} params=#{params.inspect}")
+
+      server = new(actor.id, actor.state)
       server.handle_call(action, params)
       new_state = server.state
-      record.update!(state: new_state)
-      logger.debug("[#{name}][id=#{id}] new_state = #{new_state.inspect}")
-      puts("")
-      nil
+
+      actor.update!(state: new_state)
+      message.update!(processed: true, new_state: new_state)
+      logger.debug("[#{name}][id=#{actor.id}] new_state = #{new_state.inspect}")
     end
   end
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :actor
+
+  scope :sorted, -> { order('id asc') }
+
+  scope :to_process, -> { sorted.where(processed: false) }
+
+  serialize :params
 end
